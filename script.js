8113c95a449ab732a034d26ed9363ef41dce8a6a
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2023-08-13 19:37:41 +0200

    add function markCell
    still some temporary tests elements

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const boardContainer = document.getElementById("board");
 
+//players - AI
+
+const player = (name, mark) => {
+    return {name, mark};
+};
+
+//
+
 //gameboard
 
 const gameboard = (() => {
@@ -9,12 +17,31 @@ const gameboard = (() => {
         ["empty", "empty", "empty"]
     ];
 
+    let turn = 1;
+    let player1 = player('a', 'x');
+    let player2 = player('b', 'o');
+
+    const markCell = (cell) => {
+        let mark = turn % 2 != 0 ? player1.mark : player2.mark;
+        board[cell.charAt(0)][cell.charAt(2)] = mark;
+        document.getElementById(cell).className = mark;
+        turn++;
+    };
+
     const display = () => {
+        while (boardContainer.firstChild) {
+            boardContainer.removeChild(boardContainer.firstChild);
+        }
         for (let i = 0; board[i]; i++) {
             for (let j = 0; board[i][j]; j++) {
                 let boardCell = document.createElement("div");
+                boardCell.id = i + "," + j;
                 boardCell.className = board[i][j];
                 boardContainer.append(boardCell);
+                boardCell.addEventListener('click', () => {
+                    markCell(boardCell.id);
+                    display();
+                });
             }
         }
     };
@@ -22,6 +49,6 @@ const gameboard = (() => {
     return {display};
 })();
 
-gameboard.display();
 
-//players - AI
+
+gameboard.display();
