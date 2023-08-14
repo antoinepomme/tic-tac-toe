db2d094f712415f84821d4a58ea3b37ab590ddaf
antoinepomme <140323098+antoinepomme@users.noreply.github.com>
2023-08-14 12:38:09 +0200

    Add result screen + clear button

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const mainContainer = document.getElementById("container");
 const boardContainer = document.getElementById("board");
+const resetButton = document.getElementById("reset-button");
 
 //players - AI
 
@@ -36,7 +38,7 @@ const gameboard = (() => {
          (board[0][1] === player1.mark && board [1][1] === player1.mark && board[2][1] === player1.mark) ||
          (board[0][2] === player1.mark && board [1][2] === player1.mark && board[2][2] === player1.mark) ||
          (board[0][0] === player1.mark && board [1][1] === player1.mark && board[2][2] === player1.mark) || 
-         (board[2][2] === player1.mark && board [1][1] === player1.mark && board[0][0] === player1.mark)) {
+         (board[0][2] === player1.mark && board [1][1] === player1.mark && board[2][0] === player1.mark)) {
             return "Player 1 wins !";
         } else if ((board[0][0] === player2.mark && board [0][1] === player2.mark && board[0][2] === player2.mark) ||
         (board[1][0] === player2.mark && board [1][1] === player2.mark && board[1][2] === player2.mark) ||
@@ -45,13 +47,36 @@ const gameboard = (() => {
         (board[0][1] === player2.mark && board [1][1] === player2.mark && board[2][1] === player2.mark) ||
         (board[0][2] === player2.mark && board [1][2] === player2.mark && board[2][2] === player2.mark) ||
         (board[0][0] === player2.mark && board [1][1] === player2.mark && board[2][2] === player2.mark) || 
-        (board[2][2] === player2.mark && board [1][1] === player2.mark && board[0][0] === player2.mark)) {
+        (board[0][2] === player2.mark && board [1][1] === player2.mark && board[2][0] === player2.mark)) {
             return "Player 2 wins !";
         } else if (turn == 10) {
             return "Tie";
         } else {
             return 0;
         }
+    };
+
+    const reset = () => {
+        board = [
+            ["empty", "empty", "empty"],
+            ["empty", "empty", "empty"],
+            ["empty", "empty", "empty"]
+        ];
+        turn = 1;
+        display();
+    };
+
+    const displayResult = (result) => {
+        const resultScreen = document.createElement("div");
+        resultScreen.className = "result-screen";
+        resultScreen.textContent = result;
+        document.body.append(resultScreen);
+        mainContainer.classList.toggle("blur");
+        resultScreen.addEventListener('click', () => {
+            mainContainer.classList.toggle("blur");
+            document.body.removeChild(resultScreen);
+            reset();
+        });
     };
 
     const display = () => {
@@ -73,14 +98,16 @@ const gameboard = (() => {
             }
         }
         if (getResult()) {
-            //idk
+            displayResult(getResult())
         }
  
     };
 
-    return {display};
+    return {display, reset};
 })();
 
-
+resetButton.addEventListener('click', () => {
+    gameboard.reset();
+});
 
 gameboard.display();
